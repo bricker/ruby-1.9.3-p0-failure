f71dfef4ff6043a554b45e8bf6910dc1577edcfb
Bryan Ricker <bricker88@gmail.com>
2013-08-06 16:16:23 -0700

    Add more info

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe UsersController do
+  # Control test to show that the error only occurs 
+  # when the `partial` option is used.
   it 'renders the index template' do
     get :index
     response.should render_template 'index'
   end
 
+  # Failing test
   it 'renders the partial for each user' do
     User.create(name: "Some Guy")
     get :index
